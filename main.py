076d97b674ed76bf16388bf6760effa21ce1499e
Ashwin Kumar <Ashwinash112@gmail.com>
2020-03-19 09:44:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,30 @@ app = Flask (__name__)
 
 
 @app.route("/")
-def hello():
+def home():
 
-    return  render_template("home.html")
+    return  render_template("index.html")
+
+@app.route("/archviz")
+def archviz():
+
+    return  render_template("archviz.html")
+
+@app.route("/dynamo")
+def dynamo():
+
+    return  render_template("dynamo.html")
+
+@app.route("/revitplugin")
+def revitplugin():
+
+    return  render_template("revitplugin.html")
+
+
+@app.route("/web3d")
+def web3d():
+
+    return  render_template("web3d.html")
 
 
 if __name__ == '__main__':
